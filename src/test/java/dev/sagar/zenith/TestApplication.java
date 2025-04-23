5b6a74482f780037d337e74a3e6249180a6e08d5
Sagar Nath <nathsagar96@gmail.com>
2025-04-23 23:45:13 +0530

    Add initial project structure with Maven and Spring Boot setup

---
+++ src/test/java/dev/sagar/zenith/TestApplication.java
@@ -0,0 +1,10 @@
+package dev.sagar.zenith;
+
+import org.springframework.boot.SpringApplication;
+
+public class TestApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.from(Application::main).with(TestcontainersConfiguration.class).run(args);
+  }
+}
